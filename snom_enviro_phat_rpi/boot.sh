4ca2f1693bc4ec35b982d7f3ce3409523e46049a
Alberto Sagredo <alberto@sagredo.info>
2016-09-26 21:55:00 +0200

    Update boot.sh

--- snom_enviro_phat_rpi/boot.sh
+++ snom_enviro_phat_rpi/boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i "s/HOST, PORT =.*/HOST, PORT = '', 8889/g" snom_xmlserver.py
+sed -i "s/HOST, PORT =.*/HOST, PORT = '', ${PORT}/g" snom_xmlserver.py
 
 sed -i "s,http://10.0.1.153:8888,http://${IP}:${PORT},g" snom_xmlserver.py
 
