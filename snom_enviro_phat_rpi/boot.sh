a2fb8fc3c211e49cc171b17c13dbdfcbbdbb958e
Alberto Sagredo <alberto@sagredo.info>
2016-09-26 20:06:12 +0200

    fixed sed

--- snom_enviro_phat_rpi/boot.sh
+++ snom_enviro_phat_rpi/boot.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-#IP=$(ip r | awk '/default/{print $3}')
-
-sed -i "s/HOST, PORT =.*/HOST, PORT = '${IP}\', ${PORT}/g" snom_xmlserver.py
+sed -i "s/HOST, PORT =.*/HOST, PORT = '', 8889/g" snom_xmlserver.py
 
 sed -i "s,http://10.0.1.153:8888,http://${IP}:${PORT},g" snom_xmlserver.py
 
-python /home/snom/snom_xmlserver.py 
+sudo python /home/snom/snom_xmlserver.py
 
